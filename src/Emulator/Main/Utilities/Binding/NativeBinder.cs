669b9a982aa96ade01660e57d25474b8cef1d574
mslowinski <mslowinski@internships.antmicro.com>
2021-06-16 13:17:36 +0200

    Disable pinning with GCHandle that throws error about non-primitive or non-blittable data passed as argument on NET platform

--- src/Emulator/Main/Utilities/Binding/NativeBinder.cs
+++ src/Emulator/Main/Utilities/Binding/NativeBinder.cs
@@ -58,7 +58,7 @@ namespace Antmicro.Renode.Utilities.Binding
         public NativeBinder(IEmulationElement classToBind, string libraryFile)
         {
             delegateStore = new object[0];
-#if !PLATFORM_WINDOWS
+#if !PLATFORM_WINDOWS && !NET
             handles = new GCHandle[0];
 #endif
             this.classToBind = classToBind;
@@ -87,7 +87,7 @@ namespace Antmicro.Renode.Utilities.Binding
 
         private void DisposeInner()
         {
-#if !PLATFORM_WINDOWS
+#if !PLATFORM_WINDOWS && !NET
             foreach(var handle in handles)
             {
                 handle.Free();
@@ -302,7 +302,7 @@ namespace Antmicro.Renode.Utilities.Binding
                     throw new InvalidOperationException($"Could not resolve call to managed: {e.Message}. Candidate is '{candidate}', desired method is '{desiredMethodInfo.ToString()}'");
                 }
 
-#if !PLATFORM_WINDOWS
+#if !PLATFORM_WINDOWS && !NET
                 // according to https://blogs.msdn.microsoft.com/cbrumme/2003/05/06/asynchronous-operations-pinning/,
                 // pinning is wrong (and it does not work on windows too)...
                 // but both on linux & osx it seems to be essential to avoid delegates from being relocated
@@ -395,7 +395,7 @@ namespace Antmicro.Renode.Utilities.Binding
         // which would otherwise be garbage collected while native calls
         // can still use them
         private object[] delegateStore;
-#if !PLATFORM_WINDOWS
+#if !PLATFORM_WINDOWS && !NET
         private GCHandle[] handles;
 #endif
     }
